2dafd9ff792b0c24dec4fe12ef3830c7969a6f35
Satabdi Das <satabdidas@gmail.com>
2016-05-28 13:45:31 +0530

    Put code for writing to serial monitor under a compile time flag

--- blinking_skirt.ino
+++ blinking_skirt.ino
@@ -4,6 +4,8 @@
 #include <Adafruit_Sensor.h>
 #include <Adafruit_LSM303.h>
 
+// #define DEBUG_ON
+
 Adafruit_LSM303 lsm;
 int led1 = 9;
 int led2 = 6;
@@ -14,8 +16,10 @@ int led2 = 6;
 
 void setup() 
 {
+  #ifdef DEBUG_ON
   Serial.begin(9600);
   Serial.println("Started serial monitor");
+  #endif
   
   pinMode(led1, OUTPUT);
   pinMode(led2, OUTPUT);
@@ -23,55 +27,59 @@ void setup()
   // Try to initialise and warn if we couldn't detect the chip
   if (!lsm.begin())
   {
+    #ifdef DEBUG_ON
     Serial.println("Oops ... unable to initialize the LSM303. Check your wiring!");
+    #endif
     while (1);
   }
 }
 
 void loop() 
 {
+  #ifdef DEBUG_ON
   Serial.println("Reading lsm");
+  #endif
   lsm.read();
+  #ifdef DEBUG_ON
   Serial.print("Accel X: "); Serial.print((int)lsm.accelData.x); Serial.print(" ");
   Serial.print("Y: "); Serial.print((int)lsm.accelData.y);       Serial.print(" ");
   Serial.print("Z: "); Serial.println((int)lsm.accelData.z);     Serial.print(" ");
+  #endif
 
   double storedVector = lsm.accelData.x*lsm.accelData.x;
   storedVector += lsm.accelData.y*lsm.accelData.y;
   storedVector += lsm.accelData.z*lsm.accelData.z;
   storedVector = sqrt(storedVector);
+
+  #ifdef DEBUG_ON
   Serial.print("Len: ");
   Serial.println(storedVector);
   Serial.println("Wait a bit");
+  #endif
+
   // wait a bit
   delay(1000);
  
  // get new data!
+  #ifdef DEBUG_ON
   Serial.println("Reading lsm");
+  #endif
   lsm.read();
   double newVector = lsm.accelData.x*lsm.accelData.x;
   newVector += lsm.accelData.y*lsm.accelData.y;
   newVector += lsm.accelData.z*lsm.accelData.z;
   newVector = sqrt(newVector);
+  #ifdef DEBUG_ON
   Serial.print("New Len: ");
   Serial.println(newVector);
+  #endif
   
   // are we moving 
   if (abs(newVector - storedVector) > MOVE_THRESHOLD) {
+    #ifdef DEBUG_ON
     Serial.println("Twinkle!");
+    #endif
 
-//    for (int i = 0; i <= 64; i++) {
-//      analogWrite(led1, i);
-//      analogWrite(led2, i);
-//      delay(10);
-//    }
-//    delay(100);
-//
-//    for (int i = 64; i >= 0; i--) {
-//      analogWrite(led1, i);
-//      analogWrite(led2, i);
-//      delay(10);
-//    }
     digitalWrite(led1, HIGH);
     digitalWrite(led2, HIGH);
     delay(500);
@@ -79,7 +87,9 @@ void loop()
     digitalWrite(led2, LOW);
     delay(500);
   } else {
+    #ifdef DEBUG_ON
     Serial.println("No Twinkling");
+    #endif
     delay(1000);
   }
 }
